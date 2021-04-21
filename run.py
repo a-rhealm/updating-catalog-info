fa44deaff98492893781794f35687c960465d106
Argent Rhealm <argent.rhealm@gmail.com>
2021-04-21 23:41:20 +0000

    post data to web

--- run.py
+++ run.py
@@ -3,8 +3,29 @@
 import os
 import requests
 
-path = os.path.expanduser('~') + '/supplier-data/descriptions/'
+desc_path = os.path.expanduser('~') + '/supplier-data/descriptions/'
+list_files = os.listdir(desc_path)
 
-for desc in os.listdir(path):
-	descriptions = {}
-	key = ['','','']
+img_path = os.path.expanduser('~') + '/supplier-data/images/'
+list_jpeg = [img_name for img_name in os.listdir(img_path) if '.jpeg' in img_name]
+
+list = []
+
+for file in list_files:
+
+	with open(desc_path + file, 'r') as f:
+		data = {"name": f.readline().rstrip('\n'),
+			"weight": f.readline().rstrip('\n').split()[0],
+			"description": f.readline().rstrip('\n')}
+
+	for image in list_jpeg:
+		if image.split('.')[0] in file.split('.')[0]:
+			data['image_name'] = image
+
+	list.append(data)
+
+for item in list:
+	res = requests.post("http://127.0.0.1:80/fruits/",json=item)
+
+	if not res.ok:
+		raise Exceptions("POST failed! | Status Code: {} | File: {}".format(response.status_code, f))
